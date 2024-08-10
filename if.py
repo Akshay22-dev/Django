aef1c9874a58f0a3120adcdb4a377850f696a06d
Akshay <akshayhande222@gmail.com>
2024-08-10 15:15:20 +0530

    changes

--- if.py
+++ if.py
@@ -6,9 +6,22 @@ if b>a:
     print("b is greater than a")
 '''
 
-a=10
+'''a=10
 b=20
 if a>b:
     print("a is bigger than b")
 if b>a:
     print("b is bigger than a")
+'''
+ 
+#Nested if:
+x=41
+
+if x >10:
+    print("Above ten,")
+    if x >20:
+        print("and also above 20")
+    elif x <20:
+        print("and less than 20")
+    else:
+        print("but not above 20")
